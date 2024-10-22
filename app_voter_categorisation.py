1b3e1691b517b0a460c249166e1bdf80202c942a
heaneyd <heaneyd@gmail.com>
2024-10-22 10:59:24 +0100

    Update app_voter_categorisation.py

--- app_voter_categorisation.py
+++ app_voter_categorisation.py
@@ -20,12 +20,12 @@ filtered_data = data[(data['Year'] == 2023) & (data['Award name'] == "Ballon D'O
 
 # Create a matrix where rows are journalists (Voter Name) and columns are players
 vote_matrix = filtered_data.pivot_table(index='Voter Name', columns='Player', values='Points', fill_value=0)
-vote_matrix_aggregated = vote_matrix.reshape((len(vote_matrix) // 5, 5)).mean(axis=1)
+#vote_matrix_aggregated = vote_matrix.reshape((len(vote_matrix) // 5, 5)).mean(axis=1)
 
 # Perform KMeans clustering
 num_clusters = st.sidebar.slider("Number of Clusters", min_value=2, max_value=10, value=3)
 kmeans = KMeans(n_clusters=num_clusters, random_state=0)
-kmeans_res = kmeans.fit_predict(vote_matrix_aggregated)
+kmeans_res = kmeans.fit_predict(vote_matrix)
 
 # Display the clusters
 st.write("### Journalist Clusters")
