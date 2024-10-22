099989da1570a63ae317287b32d33c6f44ba495a
heaneyd <heaneyd@gmail.com>
2024-10-22 23:04:51 +0000

    Print clusters separate, add more filters

--- app_voter_categorisation.py
+++ app_voter_categorisation.py
@@ -15,16 +15,25 @@ def load_data():
 # Load the data
 data = load_data()
 
+award_name = st.sidebar.multiselect("Award", options=data['Award name'].unique())
+if award_name:
+    data = data[data['Award name'].isin(award_name)]
+
+# Filter by Voter Role (Coach, Captain, Media)
+year = st.sidebar.multiselect("Year", options=data['Year'].unique())
+if year:
+    data = data[data['Year'].isin(year)]
+
 # Filter for 2023 Ballon D'Or
-filtered_data = data[(data['Year'] == 2023) & (data['Award name'] == "Ballon D'Or")]
+#filtered_data = data[(data['Year'] == 2023) & (data['Award name'] == "Ballon D'Or")]
 
 # Create a matrix where rows are journalists (Voter Name) and columns are players
 # 1. Calculate the total points per player
-total_points_per_player = filtered_data.groupby("Player")["Points"].sum().sort_values(ascending=False)
+total_points_per_player = data.groupby("Player")["Points"].sum().sort_values(ascending=False)
 
 # 2. Get the sorted order of players by total points
 sorted_players = total_points_per_player.index.tolist()
-vote_matrix = filtered_data.pivot_table(index='Country', columns='Player', values='Points', fill_value=0)
+vote_matrix = data.pivot_table(index='Country', columns='Player', values='Points', fill_value=0)
 vote_matrix = vote_matrix[sorted_players]
 
 #vote_matrix_aggregated = vote_matrix.reshape((len(vote_matrix) // 5, 5)).mean(axis=1)
@@ -36,16 +45,16 @@ vote_matrix['Cluster'] = kmeans.fit_predict(vote_matrix)
 
 # Display the clusters
 st.write("### Country Clusters")
-#st.write(filtered_data[['Voter Name', 'cluster']])
 st.write(vote_matrix)
 
-# Show a heatmap for the clustering
-# Sort voters by their cluster labels
-sorted_vote_matrix = vote_matrix.sort_values(by='Cluster', ascending=True)
-
-plt.figure(figsize=(10, 24))
-ax = sns.heatmap(sorted_vote_matrix.drop(columns=['Cluster']), cmap="coolwarm", cbar_kws={'label': 'Points'})
-
-
-plt.title("Country Voting Patterns Heatmap")
-st.pyplot(plt)
+st.write("### Country Clusters Heatmaps")
+for n in range(num_clusters):
+  # Show a heatmap for the clustering
+  # Sort voters by their cluster labels
+  #sorted_vote_matrix = vote_matrix.sort_values(by='Cluster', ascending=True)
+  sorted_vote_matrix = vote_matrix[(vote_matrix['Cluster'] == n)]
+  plt.figure(figsize=(10, len(sorted_vote_matrix)/4))
+  ax = sns.heatmap(sorted_vote_matrix.drop(columns=['Cluster']), cmap="coolwarm", cbar_kws={'label': 'Points'})
+  plt.title("Cluster: " + str(n))
+  st.pyplot(plt)
+  plt.clf()
