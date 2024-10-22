7b7db31cdf4923f8b1445b3fed472f2fd55b5dcd
heaneyd <heaneyd@gmail.com>
2024-10-22 10:58:06 +0100

    Update app_voter_categorisation.py

--- app_voter_categorisation.py
+++ app_voter_categorisation.py
@@ -20,18 +20,20 @@ filtered_data = data[(data['Year'] == 2023) & (data['Award name'] == "Ballon D'O
 
 # Create a matrix where rows are journalists (Voter Name) and columns are players
 vote_matrix = filtered_data.pivot_table(index='Voter Name', columns='Player', values='Points', fill_value=0)
+vote_matrix_aggregated = vote_matrix.reshape((len(vote_matrix) // 5, 5)).mean(axis=1)
 
 # Perform KMeans clustering
 num_clusters = st.sidebar.slider("Number of Clusters", min_value=2, max_value=10, value=3)
 kmeans = KMeans(n_clusters=num_clusters, random_state=0)
-filtered_data['cluster'] = kmeans.fit_predict(vote_matrix)
+kmeans_res = kmeans.fit_predict(vote_matrix_aggregated)
 
 # Display the clusters
 st.write("### Journalist Clusters")
-st.write(filtered_data[['Voter Name', 'cluster']])
+#st.write(filtered_data[['Voter Name', 'cluster']])
+st.write(kmeans_res)
 
 # Show a heatmap for the clustering
 plt.figure(figsize=(10, 8))
-sns.heatmap(vote_matrix, cmap="coolwarm", cbar_kws={'label': 'Points'})
+sns.heatmap(kmeans_res, cmap="coolwarm", cbar_kws={'label': 'Points'})
 plt.title("Journalist Voting Patterns Heatmap")
 st.pyplot(plt)
